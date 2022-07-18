60fac177308a5c4cbbe2ac417b0dccf0897b787d
Martin Tábor <ma-ta@users.noreply.github.com>
2022-07-18 19:12:16 +0200

    make

--- make_win.bat
+++ make_win.bat
@@ -3,7 +3,7 @@
 ::::::::::::::::::::::::::::::::::::::::
 ::                                    ::
 ::  SKRIPT NA SESTAVENI HRY SIBENICE  ::
-::  (BATCH\DOS)                       ::
+::  (BATCH\WINDOWS)                   ::
 ::                                    ::
 ::  autor:  Martin TABOR (Ma-TA)      ::
 ::  datum:  2022-07-18                ::
@@ -52,38 +52,38 @@ echo.
 
 :: korenovy adresar
    cd %src_dir%
-   gcc %cc_param% -c *.c
+   cl %cc_param% -c *.c
    cd ..
    move %src_dir%\*.o %out_dir%
 :: slozka game
    cd %src_dir%\game
-   gcc %cc_param% -c *.c
+   cl %cc_param% -c *.c
    cd ..\..
    move %src_dir%\game\*.o %out_dir%
 :: slozka game\ukazatele
    cd %src_dir%\game\ukazatele
-   gcc %cc_param% -c *.c
+   cl %cc_param% -c *.c
    cd ..\..\..
    move %src_dir%\game\ukazatele\*.o %out_dir%
 :: slozka help
    cd %src_dir%\help
-   gcc %cc_param% -c *.c
+   cl %cc_param% -c *.c
    cd ..\..
    move %src_dir%\help\*.o %out_dir%
 :: slozka menu
    cd %src_dir%\menu
-   gcc %cc_param% -c *.c
+   cl %cc_param% -c *.c
    cd ..\..
    move %src_dir%\menu\*.o %out_dir%
 :: slozka tui
    cd %src_dir%\tui
-   gcc %cc_param% -c *.c
+   cl %cc_param% -c *.c
    cd ..\..
    move %src_dir%\tui\*.o %out_dir%
 
 :: sestaveni spustitelneho souboru
    cd %out_dir%
-   gcc *.o -o %bin_nazev%.exe
+   cl *.o -o %bin_nazev%.exe
    del *.o
    cd ..
 
