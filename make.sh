c07cd322aee6767ce2a8bb8ee011630e9d902af7
Martin TÁBOR <ma-ta@users.noreply.github.com>
2024-03-11 17:57:39 +0100

    CC

--- make.sh
+++ make.sh
@@ -9,7 +9,7 @@
 #  (UN*X/Bash/GCC+Clang              #
 #                                    #
 #  autor:  Martin TÁBOR (Ma-TA)      #
-#  datum:  2024-02-29                #
+#  datum:  2024-03-11                #
 #                                    #
 ######################################
 
@@ -26,6 +26,9 @@
   # debug:   '-Wall -Wextra -pedantic -g -fsanitize=address'
   # release: '-O2'
   cc_param='-O2'
+
+  # příkaz pro spuštění překladače vč. parametrů (cc/gcc/clang apod.)
+  CC='cc '$cc_param
 
 # kořenový adresář se zdrojovými kódy
   src_dir=`pwd`'/source'
@@ -47,9 +50,6 @@ echo 'SESTAVUJI - Vypis chyb a udalosti:'
 echo $oramovani
 echo ''
 
-# příkaz pro spuštění překladače vč. parametrů
-  alias cc='cc '$cc_param
-
 # vytvoří adresář bin pro výstupy
   mkdir $out_dir
 # zkopíruje složku data do složky bin
@@ -62,36 +62,36 @@ echo ''
 
 # kořenový adresář
   cd $src_dir
-  cc -c *.c
+  ${CC} -c *.c
   mv *.o $out_dir
 # složka game
   cd $src_dir'/game'
-  cc -c *.c
+  ${CC} -c *.c
   mv *.o $out_dir
 # složka game/game_tui
   cd $src_dir'/game/game_tui'
-  cc -c *.c
+  ${CC} -c *.c
   mv *.o $out_dir
 # složka help
   cd $src_dir'/help'
-  cc -c *.c
+  ${CC} -c *.c
   mv *.o $out_dir
 # složka menu
   cd $src_dir'/menu'
-  cc -c *.c
+  ${CC} -c *.c
   mv *.o $out_dir
 # složka stats
   cd $src_dir'/stats'
-  cc -c *.c
+  ${CC} -c *.c
   mv *.o $out_dir
 # složka tui
   cd $src_dir'/tui'
-  cc -c *.c
+  ${CC} -c *.c
   mv *.o $out_dir
 
 # sestavení spustitelného souboru
   cd $out_dir
-  cc *.o -o $bin_nazev
+  ${CC} *.o -o $bin_nazev
   rm *.o
 
 echo ''
