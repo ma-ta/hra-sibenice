2e49f820a3a9b3d7926fb38a91fd4acd39b6b76b
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-03-29 13:26:10 +0100

    Update term_set.bat

--- source/dev/term_set.bat
+++ source/dev/term_set.bat
@@ -3,6 +3,11 @@
 @REM na Windows (vč. přepínačů a závislostí)
 @REM (v term_set.c nastavit DEBUG na 1 pro test modulu)
 
-cl.exe /c term_set.c
-cl.exe *.obj term_main.c /Feterm_win.exe /link /subsystem:windows /entry:mainCRTStartup user32.lib
+@REM Pro kompilaci term_main.c a term_set.c zvlášt (test použití jako modulu programu)
+@REM cl.exe /c term_set.c
+@REM cl.exe *.obj term_main.c /Feterm_win.exe /link /subsystem:windows /entry:mainCRTStartup user32.lib
+
+@REM Samostatná kompilace modulu (DEBUG == 1) - obsahuje fci. main() s testy
+cl.exe term_set.c /Feterm_win.exe /link /subsystem:windows /entry:mainCRTStartup user32.lib
+
 del *.obj
