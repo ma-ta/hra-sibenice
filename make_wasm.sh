ec5427f80a097740eaeaf7534d0cb1efa8989081
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-06-09 08:25:02 +0200

    -

--- make_wasm.sh
+++ make_wasm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Konce radku v souboru musi byt reprezentovany znakem LF ( !! nikoli CRLF !! )
+
 # Testovane prostredi pro sestaveni:
 #   - Ubuntu 24.04.2 LTS + Emscripten v4.0.9,
 #   - npm v11.4.1, xterm v5.5.0, xterm-pty v0.11.1
