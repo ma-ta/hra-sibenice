72a2b178d61ae66e9a6ab7a589415591afeb1746
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-06-10 11:07:19 +0200

    test
    Testovací soubor k implementaci UN*X části funkce term_size() v modulu term_set.c

---
+++ source/libs/pokus.c
@@ -0,0 +1,53 @@
+/*
+ *  (TESTOVACÍ SOUBOR - rozpracováno)
+ *
+ *  Nastavení velikosti okna emulátoru (GNOME Terminal, ...)
+ *  v UN*X systémech -> k implementaci do term_set.c
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#define SIRKA  74
+#define VYSKA  34
+
+int main(void)
+{
+    int vychozi_x, vychozi_y;
+    struct winsize w;
+
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+    vychozi_y = w.ws_row;
+    vychozi_x = w.ws_col;
+
+    system("clear");
+
+    printf(
+      "Okno ma vysku: %d radku a sirku %d sloupecku.\n"
+      , vychozi_y
+      , vychozi_x
+    );
+
+    printf(
+      "Nastavuji velikost %d x %d zn.\n"
+      "(stiskni Enter...)"
+      , SIRKA
+      , VYSKA
+    );
+
+    /* řídicí sekvence XTerm */
+    printf("\033[8;%d;%dt", VYSKA, SIRKA);
+    fflush(stdout);
+
+    while (getchar() != '\n')
+      ;  /* čekání na Enter */
+
+    /* navrácení původní velikosti - při spuštění programu */
+    printf("\033[8;%d;%dt", vychozi_y, vychozi_x);
+    fflush(stdout);
+
+    return 0;
+}
