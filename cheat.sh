c7bc8b4d914bf0df59584c339f7679e4fe23a7b2
Martin TÁBOR <ma-ta@users.noreply.github.com>
2025-10-22 14:18:22 +0200

    -

---
+++ cheat.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Konce radku v souboru musi byt reprezentovany znakem LF ( !! nikoli CRLF !! )
+
+# Cheat pro vypsani aktualne hadaneho slova
+# Použití: ./cheat.sh <nazev_procesu>
+# (spustit v průběhu hry)
+
+
+if [ -z "$1" ]; then
+    echo "Pouziti: $0 <nazev_procesu>"
+    echo
+fi
+
+PID=$(pgrep -fo "${1:-test-unix}")
+
+if [[ -n "$PID" ]]; then
+    echo pid: $PID
+    gdb --batch -p $PID -ex 'x/s &hadane_slovo' -ex quit | grep hadane_slovo
+else
+    echo "(!) Proces nebyl nalezen..." >&2
+    exit 1
+fi
