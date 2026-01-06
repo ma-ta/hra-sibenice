1964d1f2ba2f4ff662cde7645b57000f1bfa66a1
ma-ta <ma-ta@users.noreply.github.com>
2026-01-06 20:58:24 +0100

    -

--- cheat.sh
+++ cheat.sh
@@ -16,7 +16,7 @@
 # Verze        :  Hra Šibenice v1.3.2
 # Testovane OS :  Ubuntu 24.04.3 LTS, Fedora 43
 # Autor        :  Martin TABOR (Ma-TA)
-# Upraveno     :  2025-11-01
+# Upraveno     :  2026-01-07
 
 
 # kontrola pritomnosti argumentu <nazev_procesu>
